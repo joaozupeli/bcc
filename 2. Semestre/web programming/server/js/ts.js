cccb19b60a08f54fa6f740d0e2e463a49dbfa7fd
Joao francisco Azevedo Zupeli <joaozupeli7@gmail.com>
2026-09-05 14:42:24 -0300

    learning php

--- 2. Semestre/web programming/server/js/ts.js
+++ 2. Semestre/web programming/server/js/ts.js
@@ -1,7 +1,15 @@
 fetch("https://localhost:8000/",{
-    method: "POST"
-    headers
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        name: "John Doe",
+        email: "john.doe@example.com"
+    })
 })
-
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
 
 
